a2202757d9fea8afb6aa2251dcf11cbb61bcb30a
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-06-13 20:18:12 +0530

    variables in java

---
+++ core-java/variable.java
@@ -0,0 +1,8 @@
+class variable{
+    public static void main(String a[]){
+        int num = 5;
+        int num1 = 6;
+        int result = num + num1;
+        System.out.println(result);
+    }
+}
