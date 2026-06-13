d8e3bb3259d57c112ff6a5ca270c6e65b4730386
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-06-13 20:28:27 +0530

    datatypes in java

---
+++ core-java/DataTypes.java
@@ -0,0 +1,37 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        // Integer Types
+        byte by = 127;
+        short sh = 558;
+        int num1 = 9;
+        long l = 69621L;
+
+        // Floating Point Types
+        float f = 6.5f;
+        double d = 5.8;
+
+        // Character Type
+        char c = 'K';
+        char ch = '8';
+
+        // Boolean Type
+        boolean b = true;
+
+        // Printing Values
+        System.out.println("===== JAVA DATA TYPES =====");
+
+        System.out.println("byte value    : " + by);
+        System.out.println("short value   : " + sh);
+        System.out.println("int value     : " + num1);
+        System.out.println("long value    : " + l);
+
+        System.out.println("float value   : " + f);
+        System.out.println("double value  : " + d);
+
+        System.out.println("char value    : " + c);
+        System.out.println("char value    : " + ch);
+
+        System.out.println("boolean value : " + b);
+    }
+}
