7f88e6ffa60f5fde29d1eac4c9b7281ddf8e21dc
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-06-14 18:05:42 +0530

    relational operators in java

---
+++ core-java/relationalOperator.java
@@ -0,0 +1,43 @@
+public class relationalOperator {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 3;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("\n===== Relational Operators =====");
+
+        // Greater Than
+        System.out.println("a > b  : " + (a > b));
+
+        // Less Than
+        System.out.println("a < b  : " + (a < b));
+
+        // Greater Than or Equal To
+        System.out.println("a >= b : " + (a >= b));
+
+        // Less Than or Equal To
+        System.out.println("a <= b : " + (a <= b));
+
+        // Equal To
+        System.out.println("a == b : " + (a == b));
+
+        // Not Equal To
+        System.out.println("a != b : " + (a != b));
+
+        double c = 8.8;
+        double d = 8.8;
+
+        System.out.println("\nc = " + c);
+        System.out.println("d = " + d);
+
+        System.out.println("c == d : " + (c == d));
+        System.out.println("c != d : " + (c != d));
+        System.out.println("c > d  : " + (c > d));
+        System.out.println("c < d  : " + (c < d));
+        System.out.println("c >= d : " + (c >= d));
+        System.out.println("c <= d : " + (c <= d));
+    }
+}
