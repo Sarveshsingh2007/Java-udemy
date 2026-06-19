72e17d57ce1fb03df8d6e6240c9dc3d782075dbd
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-06-19 12:47:02 +0530

    encapsulation in java

---
+++ core-java/encapsulation.java
@@ -0,0 +1,26 @@
+class Human{
+    private int age;
+    private String name;
+
+    public void setAge(int a){
+        age = a;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setName(String n){
+        name = n;
+    }
+    public String getName(){
+        return name;
+    }
+}
+
+public class encapsulation {
+    public static void main(String[] args){
+        Human obj = new Human();
+        obj.setAge(20);
+        obj.setName("Sarvesh");
+        System.out.println(obj.getName() + " : " + obj.getAge());
+    }
+}
