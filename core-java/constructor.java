d3f3cbe9707c58995db841bc92e76b920db5d1b3
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-06-19 13:20:03 +0530

    default and parameterized constructor in java

--- core-java/constructor.java
+++ core-java/constructor.java
@@ -2,10 +2,15 @@ class Human{
     private int age;
     private String name;
 
-    // constructor
+    // default constructor
     public Human(){
         age = 20;
         name = "Sarvesh";
+    }
+    // parameterised constructor
+    public Human(int age, String name){
+        this.age = age;
+        this.name = name;
     }
 
     public void setAge(int age){ // setter 
@@ -25,6 +30,8 @@ class Human{
 public class constructor {
     public static void main(String[] args){
         Human obj = new Human();
+        Human obj1 = new Human(10, "jhon");
         System.out.println(obj.getName() + " : " + obj.getAge());
+        System.out.println(obj1.getName() + " : " + obj1.getAge());
     }
 }
