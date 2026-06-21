d899abf6ec08e70bef6699ed88fbe98f575d7966
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-06-21 22:26:50 +0530

    updated VeryAdvCalc for multilevel inheritance

--- core-java/inheritance/calculator.java
+++ core-java/inheritance/calculator.java
@@ -2,11 +2,13 @@ package inheritance;
 
 public class calculator {
     public static void main(String a[]){
-    AdvCalc obj = new AdvCalc();
+    VeryAdvCalc obj = new VeryAdvCalc();
     int r1 = obj.add(4,5);
     int r2 = obj.subtract(5,4);
     int r3 = obj.mul(2,3);
     int r4 = obj.div(10,2);
-    System.out.println(r1 + " " + r2 + " " + r3 + " " + r4);
+    double r5 = obj.power(2,4);
+
+    System.out.println(r1 + " " + r2 + " " + r3 + " " + r4 + " " + r5);
     }
 }
