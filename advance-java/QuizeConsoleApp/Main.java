e2c7618f9772746b0394c322ab4c5f48f8131c88
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-06-25 01:26:40 +0530

    updated playQuiz() and added printScore()

--- advance-java/QuizeConsoleApp/Main.java
+++ advance-java/QuizeConsoleApp/Main.java
@@ -3,6 +3,7 @@ package QuizeConsoleApp;
 public class Main{
     public static void main(String[] args){
         QuestionService service = new QuestionService();
-        service.displayQuestions();
+        service.playQuiz();
+        service.printScore();
     }
 }
--- advance-java/QuizeConsoleApp/QuestionService.java
+++ advance-java/QuizeConsoleApp/QuestionService.java
@@ -1,7 +1,9 @@
 package QuizeConsoleApp;
+import java.util.Scanner;
 
 public class QuestionService {
     Question[] questions = new Question[5];
+    String selection[] = new String[5];
     public QuestionService(){
         questions[0] = new Question(1, "size of int", "2", "6", "4", "8", "4");
         questions[1] = new Question(2, "size of double", "2", "6", "4", "8", "8");
@@ -10,10 +12,34 @@ public class QuestionService {
         questions[4] = new Question(5, "size of boolean", "1", "2", "4", "8", "1");
     }
     
-    public void displayQuestions(){
+    public void playQuiz(){
+        int i=0;
+        Scanner sc = new Scanner(System.in); 
         for(Question q : questions){
-            System.out.println(q);
+            System.out.println("Question no." + q.getId() + ": "+ q.getQuestion());
+            System.out.println("1: " +q.getOpt1());
+            System.out.println("2: " +q.getOpt2());
+            System.out.println("3: " +q.getOpt3());
+            System.out.println("4: " +q.getOpt4());
+            System.out.print("Choose answer: ");
+            selection[i] = sc.nextLine();
+            i++;
         }
-        
+        for(String s : selection){
+            System.out.println("Selected option for questions:" + s);
+        }
     } 
+
+    public void printScore(){
+        int score = 0;
+        for(int i=0;i<questions.length;i++){
+            Question que = questions[i];
+            String actualAnswer = que.getAnswer();
+            String userAnswer = selection[i];
+            if(actualAnswer.equals(userAnswer)){
+                score++;
+            }
+        }
+        System.out.println("Your Score: " + score);
+    }
 }
