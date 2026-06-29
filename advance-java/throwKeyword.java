41b8e00f01a90a6ad268e1714b408d03e1c6bd1e
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-06-29 20:46:04 +0530

    throw keyword in exception handling

---
+++ advance-java/throwKeyword.java
@@ -0,0 +1,17 @@
+public class throwKeyword {
+    public static void main(String[] args){
+        int i = 20;
+        int j = 0;
+
+        try{
+            j = 18/i;
+            if(j==0)
+                throw new ArithmeticException("don't want to print 0.");
+        }
+        catch(ArithmeticException e){
+            j = 18/1;
+            System.out.println("Its a default output.");
+        }
+        System.out.println("Bye");
+    }
+}
