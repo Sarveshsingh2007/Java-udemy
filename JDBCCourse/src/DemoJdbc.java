1b563622845461fba6c9459e777a1ce81dc2a485
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-07-05 16:05:46 +0530

    creating and executing statement and closing con.close()

--- JDBCCourse/src/DemoJdbc.java
+++ JDBCCourse/src/DemoJdbc.java
@@ -1,5 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DemoJdbc {
     public static void main(String[] args) throws Exception {
@@ -14,6 +16,13 @@ public class DemoJdbc {
         Connection con = DriverManager.getConnection(url, uname, pass);
 
         System.out.println("Connected to database successfully");
-
+        String sql = "select sname from student where sid = 1";
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        rs.next();
+        String name = rs.getString("sname");
+        System.out.println("Student name is: " + name);
+        con.close();
+        System.out.println("Connection closed");
     }
 }
