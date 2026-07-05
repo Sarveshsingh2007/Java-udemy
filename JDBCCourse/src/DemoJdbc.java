4c2cdb49b3debd065fa3600983b1ce3f510172ed
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-07-05 17:20:35 +0530

    updated prepared Statement (?, ?, ?)

--- JDBCCourse/src/DemoJdbc.java
+++ JDBCCourse/src/DemoJdbc.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class DemoJdbc {
     public static void main(String[] args) throws Exception {
@@ -20,17 +17,30 @@ public class DemoJdbc {
 //        String sql = "insert into student values (5, 'himanshu', 45)";      // inserting value
 //        String sql = "update student set sname='shiva' where sid  = 5";       // updating value
 //        String sql = "delete from student where sid = 5";    // deleting row
-        int sid = 5;
-        String sname = "shiva";
-        int marks = 45;
 
-        String sql = "insert into student values("+ sid + ", '" + sname +"', "+ marks +")";  // very difficult to write
+        int sid = 6;
+        String sname = "chanchal";
+        int marks = 54;
+//
+//        String sql = "insert into student values
+//                      ("+ sid + ", '" + sname +"', "+ marks +")";  // very difficult to write
+        // this can damage out database- sql injection
+        // string concatenation - As queries become longer, they become harder to manage.
+
+        String sql = "insert into student values(?, ?, ?)";
 
 
-        Statement st = con.createStatement();
+//        Statement st = con.createStatement();
 //        ResultSet rs = st.executeQuery(sql);
 
-        st.execute(sql);
+        // PREPARED STATEMENT
+        PreparedStatement st = con.prepareStatement(sql);
+        st.setInt(1, sid);
+        st.setString(2,sname);
+        st.setInt(3, marks);
+        st.execute();
+
+//        st.execute(sql);
 //        System.out.println("Updated successfully");
 //        System.out.println("Deleted row 5 successfully");
 
