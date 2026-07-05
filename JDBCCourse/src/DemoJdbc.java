b6d2c76bd5bd1e17474117f5aec1f6f67184f1c0
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-07-05 17:05:13 +0530

    CRUD operations updated- create, insert, update, delete

--- JDBCCourse/src/DemoJdbc.java
+++ JDBCCourse/src/DemoJdbc.java
@@ -11,17 +11,40 @@ public class DemoJdbc {
 
         String url = "jdbc:postgresql://localhost:5432/Demo";
         String uname = "postgres";
-        String pass = "######";
-
+        String pass = "#####";
         Connection con = DriverManager.getConnection(url, uname, pass);
-
         System.out.println("Connected to database successfully");
-        String sql = "select sname from student where sid = 1";
+
+
+//        String sql = "select * from student"; // fecthing all values
+//        String sql = "insert into student values (5, 'himanshu', 45)";      // inserting value
+//        String sql = "update student set sname='shiva' where sid  = 5";       // updating value
+//        String sql = "delete from student where sid = 5";    // deleting row
+        int sid = 5;
+        String sname = "shiva";
+        int marks = 45;
+
+        String sql = "insert into student values("+ sid + ", '" + sname +"', "+ marks +")";  // very difficult to write
+
+
         Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(sql);
-        rs.next();
-        String name = rs.getString("sname");
-        System.out.println("Student name is: " + name);
+//        ResultSet rs = st.executeQuery(sql);
+
+        st.execute(sql);
+//        System.out.println("Updated successfully");
+//        System.out.println("Deleted row 5 successfully");
+
+
+//        rs.next();
+//        String name = rs.getString("sname");
+//        System.out.println("Student name is: " + name);
+
+//        while(rs.next()){
+//            System.out.print(rs.getInt( 1) + " - ");
+//            System.out.print(rs.getString(2) + " - ");
+//            System.out.println(rs.getString(3));
+//        }
+
         con.close();
         System.out.println("Connection closed");
     }
