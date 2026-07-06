0e65df5e1179fc620b9a8d28d31f6741f0df4c2d
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-07-06 16:43:17 +0530

    Added Hibernate entity and saved data

---
+++ HibernateProject/src/main/java/com/sarvesh/Main.java
@@ -0,0 +1,29 @@
+package com.sarvesh;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setRollNo(1);
+        s1.setsName("sarvesh");
+        s1.setsAge(21);
+
+        Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(com.sarvesh.Student.class);
+        cfg.configure();
+
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+        session.persist(s1);
+        tx.commit();
+        System.out.println(s1);
+    }
+}
