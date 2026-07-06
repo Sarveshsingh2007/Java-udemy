78fbdd8aece90d944087301723a9b43929619ccc
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-07-06 17:54:28 +0530

    updated the table name and column names

--- HibernateProject/src/main/java/com/sarvesh/Main.java
+++ HibernateProject/src/main/java/com/sarvesh/Main.java
@@ -7,23 +7,58 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
-    public static void main(String[] args) {
-        Student s1 = new Student();
-        s1.setRollNo(1);
-        s1.setsName("sarvesh");
-        s1.setsAge(21);
+    static void main(String[] args) {
+//        Student s1 = new Student();
+//        s1.setRollNo(3);
+//        s1.setsName("shiva");
+//        s1.setsAge(15);
 
-        Configuration cfg = new Configuration();
-        cfg.addAnnotatedClass(com.sarvesh.Student.class);
-        cfg.configure();
+//        Student s2 = null;
 
-        SessionFactory sf = cfg.buildSessionFactory();
+//        Configuration cfg = new Configuration();
+//        cfg.addAnnotatedClass(com.sarvesh.Student.class);
+//        cfg.configure();
+
+//        SessionFactory sf = new Configuration()               // cfg.buildSessionFactory();
+//                .addAnnotatedClass(com.sarvesh.Student.class)
+//                .configure()
+//                .buildSessionFactory();
+
+//        Session session = sf.openSession();
+//        s2 = session.find(Student.class, 1);                 // fetch data
+
+//        Transaction tx = session.beginTransaction();
+
+//        session.merge(s1);           // merge() update data
+//        session.persist(s1);           //create
+//        s1 = session.find(Student.class, 2);
+//        session.remove(s1);
+
+//        tx.commit();
+//        session.close();
+//        sf.close();
+
+//        System.out.println(s1);
+//        System.out.println(s2);       fetch data
+
+        Alien a1 = new Alien();
+        a1.setAid(1);
+        a1.setAname("Jadu");
+        a1.setTech("space");
+
+        SessionFactory sf= new Configuration()
+                .addAnnotatedClass(Alien.class)
+                .configure()
+                .buildSessionFactory();
+
         Session session = sf.openSession();
 
         Transaction tx = session.beginTransaction();
 
-        session.persist(s1);
+        session.persist(a1);
         tx.commit();
-        System.out.println(s1);
+
+        session.close();
+        sf.close();
     }
 }
