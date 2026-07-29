259924cd1a7fed63d102e082f5aa5ef50c484fa1
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-07-30 02:01:24 +0530

    Add Spring JDBC example

---
+++ SpringJdbcEx/src/main/java/com/sarvesh/SpringJdbcEx/SpringJdbcExApplication.java
@@ -0,0 +1,28 @@
+package com.sarvesh.SpringJdbcEx;
+
+import com.sarvesh.SpringJdbcEx.model.Student;
+import com.sarvesh.SpringJdbcEx.service.StudentService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+@SpringBootApplication
+public class SpringJdbcExApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringJdbcExApplication.class, args);
+        Student s = context.getBean(Student.class);
+		s.setRollNo(101);
+		s.setName("Sarvesh");
+		s.setMarks(95);
+
+		StudentService service = context.getBean(StudentService.class);
+		service.addStudent(s);
+
+		List<Student> students = service.getStudents();
+		System.out.println(students);
+	}
+
+}
---
+++ SpringJdbcEx/src/main/java/com/sarvesh/SpringJdbcEx/service/StudentService.java
@@ -0,0 +1,31 @@
+package com.sarvesh.SpringJdbcEx.service;
+
+import com.sarvesh.SpringJdbcEx.model.Student;
+import com.sarvesh.SpringJdbcEx.repo.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    private StudentRepo repo;
+
+    public StudentRepo getRepo() {
+        return repo;
+    }
+
+    @Autowired
+    public void setRepo(StudentRepo repo) {
+        this.repo = repo;
+    }
+
+    public void addStudent(Student s) {
+        repo.save(s);
+    }
+
+    public List<Student> getStudents() {
+        return repo.findAll();
+    }
+}
