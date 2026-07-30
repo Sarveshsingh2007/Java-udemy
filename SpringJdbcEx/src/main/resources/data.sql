4e21a7c8177f522718330cc3d56dfcbb50313cfd
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-07-30 12:13:23 +0530

    data.sql for h2

---
+++ SpringJdbcEx/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into Student (RollNo, Name, Marks) values (101, 'Himanshu', 79);
+insert into Student (ROllNo, Name, Marks) values (102, 'Shivam', 84);
+insert into Student (RollNo, Name, Marks) values (103, 'Sahil', 69);
