9da246a76771966bf4823409c74f7656992878ca
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-08-03 23:57:56 +0530

    Servlet in java

--- ServletsEx/src/main/java/com/sarvesh/HelloServlet.java
+++ ServletsEx/src/main/java/com/sarvesh/HelloServlet.java
@@ -3,9 +3,15 @@ package com.sarvesh;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloServlet extends HttpServlet {
-    public void service(HttpServletRequest req, HttpServletResponse response){
+    public void service(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h2><b>Hello Sarvesh!</b></h2>");
+
         System.out.println("In Service");
     }
 }
