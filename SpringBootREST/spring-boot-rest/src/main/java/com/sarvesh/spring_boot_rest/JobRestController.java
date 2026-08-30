b2ca9488f2036fe81bfe3d512bea41a38786a7a6
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-08-30 18:03:29 +0530

    added new dependencies in pom.xml for jakson format xml

--- SpringBootREST/spring-boot-rest/src/main/java/com/sarvesh/spring_boot_rest/JobRestController.java
+++ SpringBootREST/spring-boot-rest/src/main/java/com/sarvesh/spring_boot_rest/JobRestController.java
@@ -14,7 +14,7 @@ public class JobRestController {
     @Autowired
     private JobService service;
 
-    @GetMapping("jobPosts")
+    @GetMapping(path="jobPosts", produces = "application/json")
     public List<JobPost> getAllJobs(){
         return service.getAllJobs();
     }
