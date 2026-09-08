77fc85fce5b1c61da5061fdc9065b599cee53d46
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-09-08 10:58:54 +0530

    created update and delete controller

--- Project-Using-Spring-Boot-MVC/SringEcom/src/main/java/com/sarvesh/SringEcom/Service/ProductService.java
+++ Project-Using-Spring-Boot-MVC/SringEcom/src/main/java/com/sarvesh/SringEcom/Service/ProductService.java
@@ -29,4 +29,19 @@ public class ProductService {
         product.setImageData(image.getBytes());
         return productRepo.save(product);
     }
+
+    public Product updateProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return productRepo.searchProducts(keyword);
+    }
 }
