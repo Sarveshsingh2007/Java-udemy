fad2ae1f0e8c9e151df9536fcae6f1fb8a19ac1a
Sarvesh singh <sarveshadhikari905@gmail.com>
2026-09-17 21:16:44 +0530

    updated placeOrder(), getAllOrderResponses() service

--- Project-Using-Spring-Boot-MVC/SringEcom/src/main/java/com/sarvesh/SringEcom/Repo/OrderRepo.java
+++ Project-Using-Spring-Boot-MVC/SringEcom/src/main/java/com/sarvesh/SringEcom/Repo/OrderRepo.java
@@ -3,9 +3,11 @@ package com.sarvesh.SringEcom.Repo;
 import com.sarvesh.SringEcom.Model.Order;
 import com.sarvesh.SringEcom.Model.dto.OrderResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface OrderRepo extends JpaRepository<Order, Integer> {
-    OrderResponse findByOrderId(String orderId);
+    Optional<Order> findByOrderId(String orderId);
 }
--- Project-Using-Spring-Boot-MVC/SringEcom/src/main/java/com/sarvesh/SringEcom/Service/OrderService.java
+++ Project-Using-Spring-Boot-MVC/SringEcom/src/main/java/com/sarvesh/SringEcom/Service/OrderService.java
@@ -1,6 +1,10 @@
 package com.sarvesh.SringEcom.Service;
 
 import com.sarvesh.SringEcom.Model.Order;
+import com.sarvesh.SringEcom.Model.OrderItem;
+import com.sarvesh.SringEcom.Model.Product;
+import com.sarvesh.SringEcom.Model.dto.OrderItemRequest;
+import com.sarvesh.SringEcom.Model.dto.OrderItemResponse;
 import com.sarvesh.SringEcom.Model.dto.OrderRequest;
 import com.sarvesh.SringEcom.Model.dto.OrderResponse;
 import com.sarvesh.SringEcom.Repo.OrderRepo;
@@ -8,7 +12,9 @@ import com.sarvesh.SringEcom.Repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,24 +23,89 @@ public class OrderService {
 
     @Autowired
     private ProductRepo productRepo;
+
+    @Autowired
     private OrderRepo orderRepo;
 
-    public OrderResponse placeOrder(OrderRequest orderRequest) {
+    public OrderResponse placeOrder(OrderRequest request) {
         Order order = new Order();
-        String orderId = UUID.randomUUID()
-                .toString()
-                .substring(0, 8)
-                .toUpperCase();
-
+        String orderId = "ORD" + UUID.randomUUID().toString().substring(0, 8).toUpperCase();
         order.setOrderId(orderId);
-        order.setCustomerName(orderRequest.customerName());
-        order.setEmail(orderRequest.email());
+        order.setCustomerName(request.customerName());
+        order.setEmail(request.email());
         order.setStatus("Placed");
         order.setOrderDate(LocalDate.now());
-        return orderRepo.findByOrderId(orderId);
+
+        List<OrderItem> orderItems = new ArrayList<>();
+
+        for (OrderItemRequest itemReq : request.items()) {
+            Product product = productRepo.findById(itemReq.productId())
+                    .orElseThrow(() -> new RuntimeException("Product Not Found"));
+
+            product.setStockQuantity(product.getStockQuantity() - itemReq.quantity());
+            productRepo.save(product);
+
+            OrderItem orderItem = OrderItem.builder()
+                    .product(product)
+                    .quantity(itemReq.quantity())
+                    .TotalPrice(product.getPrice().multiply(BigDecimal.valueOf(itemReq.quantity())))
+                    .order(order)
+                    .build();
+
+            orderItems.add(orderItem);
+        }
+
+        order.setOrderItems(orderItems);
+        Order savedOrder = orderRepo.save(order);
+
+        List<OrderItemResponse> itemResponses = new ArrayList<>();
+
+        for (OrderItem item : order.getOrderItems()) {
+            OrderItemResponse orderItemResponse = new OrderItemResponse(
+                    item.getProduct().getName(),
+                    item.getQuantity(),
+                    item.getTotalPrice()
+            );
+
+            itemResponses.add(orderItemResponse);
+        }
+
+        OrderResponse orderResponse = new OrderResponse(
+                savedOrder.getOrderId(),
+                savedOrder.getCustomerName(),
+                savedOrder.getEmail(),
+                savedOrder.getStatus(),
+                savedOrder.getOrderDate(),
+                itemResponses
+        );
+        return orderResponse;
     }
 
     public List<OrderResponse> getAllOrderResponses() {
-        return null;
+        List<Order> orders = orderRepo.findAll();
+        List<OrderResponse> orderResponses = new ArrayList<>();
+
+        for(Order order : orders){
+            List<OrderItemResponse> itemResponses = new ArrayList<>();
+            for(OrderItem item : order.getOrderItems()){
+                OrderItemResponse  itemResponse = new OrderItemResponse(
+                        item.getProduct().getName(),
+                        item.getQuantity(),
+                        item.getTotalPrice()
+                );
+                itemResponses.add(itemResponse);
+            }
+            OrderResponse orderResponse = new OrderResponse(
+                    order.getOrderId(),
+                    order.getCustomerName(),
+                    order.getEmail(),
+                    order.getStatus(),
+                    order.getOrderDate(),
+                    itemResponses
+            );
+            orderResponses.add(orderResponse);
+        }
+
+        return orderResponses;
     }
 }
